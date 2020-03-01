9cf79ce345c1a8fa8416eaaed4f870af025b5d34
Abner Andino <brain.agam@gmail.com>
2020-03-02 00:32:56 +0100

    TASK factories for our models

--- immfly/common/factories.py
+++ immfly/common/factories.py
@@ -1,9 +1,13 @@
 import factory
 
 from common.models import Language
+from utils import random_letters
 
 
 class LanguageFactory(factory.DjangoModelFactory):
 
     class Meta:
-        # model = 
+        model = Language
+
+    code = factory.LazyAttribute(lambda x: random_letters(2))
+    name = factory.LazyAttribute(lambda x: random_letters(10))
