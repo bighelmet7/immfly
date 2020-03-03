1ae2b51d6d949393b5487c39e9fd4d436e105d63
Abner Andino <brain.agam@gmail.com>
2020-03-03 20:27:32 +0100

    TASK: Admin view for Channel, Tree and content

--- immfly/channel/admin.py
+++ immfly/channel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from channel.models import ChannelNode, Content, TreeChannel
+
+@admin.register(TreeChannel)
+class TreeChannelAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name', # This is made because we want to avoid the __str__ method.
+    )
+
+admin.site.register(ChannelNode)
+admin.site.register(Content)
