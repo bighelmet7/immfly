215bf6df86b727d532721d1485c84f4c277b6da3
Abner Andino <brain.agam@gmail.com>
2020-03-03 20:32:45 +0100

    TASK: str representation for language model

--- immfly/common/models.py
+++ immfly/common/models.py
@@ -8,8 +8,11 @@ class Language(models.Model):
     """
 
     code = models.CharField(max_length=2, primary_key=True, unique=True)
-    name = models.TextField(max_length=150, unique=True)
+    name = models.CharField(max_length=50, unique=True)
 
     def save(self, *args, **kwargs):
         self.code = self.code.lower()
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return '%s - %s' % (self.code, self.name)
