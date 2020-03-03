3b883e43e4e36499608fac5da008b60ee30867b0
Abner Andino <brain.agam@gmail.com>
2020-03-03 20:29:38 +0100

    TASK: str representation for all models and history rating values for ChannelNode

--- immfly/channel/models.py
+++ immfly/channel/models.py
@@ -1,5 +1,4 @@
-from decimal import Decimal
-from queue import Queue
+from decimal import Decimal, localcontext
 
 from django.contrib.postgres.fields import JSONField
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -15,7 +14,10 @@ class TreeChannel(models.Model):
     entertainments system, containing the parent root.
     """
 
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
+
+    def __str__(self):
+        return '<TreeChannel %s> %s' % (self.id, self.name)
 
 
 class ChannelNode(models.Model):
@@ -27,11 +29,11 @@ class ChannelNode(models.Model):
     have set the attribute contents.
     """
 
-    root = models.ForeignKey(
+    tree = models.ForeignKey(
         'TreeChannel',
         blank=True,
         null=True,
-        related_name='tree',
+        related_name='root',
         on_delete=models.CASCADE
     )
 
@@ -43,7 +45,7 @@ class ChannelNode(models.Model):
         on_delete=models.CASCADE
     )
 
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique=True)
     language = models.ForeignKey(Language, on_delete=models.PROTECT)
     picture = models.ImageField(blank=True)
     thumbnail = models.ImageField(blank=True, height_field=100, width_field=100)
@@ -55,29 +57,49 @@ class ChannelNode(models.Model):
 
         super().save(*args, **kwargs)
 
-    def _irate_value(self, node):
+    def _irate_value(self, node, calculated_values):
         """
         Base case: if node is a leaf then returns the AVG of its contents.
         Induction: the Avg of a parent node depends on the avg of its children.
-        """
-        if not node.children.count():
-            content_avg = node.contents.all().aggregate(Avg('rating_value'))
-            return content_avg['rating_value__avg']
 
-        n_nodes = 0
-        _rate_sum = Decimal('0')
-        for child in node.children.all():
-            n_nodes += child.children.count()
-            _rate_sum += self._irate_value(child)
-        n_nodes += node.children.count()
-        return Decimal(_rate_sum / n_nodes)
+        *Returns the AVG of the current node.
+
+        *Modifies a dict passed by reference where contains as a key the node id and
+        as a value the AVG of the node.
+        """
+        with localcontext() as ctx: # This is need for Decimal standarization.
+            ctx.prec = 4
+            if not node.children.count():
+                content_avg = node.contents.all().aggregate(Avg('rating_value'))
+                result = content_avg['rating_value__avg']
+                calculated_values[node.title] = result
+                return result
+
+            _rate_sum = Decimal('0')
+            for child in node.children.all():
+                _rate_sum += self._irate_value(child, calculated_values)
+
+            result = Decimal(_rate_sum / node.children.count())
+            calculated_values[node.title] = result
+            return result
 
     @property
     def rating_value(self):
         """
         Calculate the rating value of a channel.
+
+        Return the rating value of the given node, and an history
+        of all the calculated rating values of the hierarchy of the
+        current node.
         """
-        return self._irate_value(self)
+
+        calculated_values = dict()
+        result = self._irate_value(self, calculated_values)
+
+        return result, calculated_values
+
+    def __str__(self):
+        return '<ChannelNode %s> %s' % (self.id, self.title)
 
 
 class Content(models.Model):
@@ -87,7 +109,7 @@ class Content(models.Model):
     below to it.
     """
 
-    item = models.FileField()
+    item = models.FileField(blank=True)
     meta_info = JSONField(blank=True, null=True)
     rating_value = models.DecimalField(
         default=Decimal('0'),
@@ -97,8 +119,12 @@ class Content(models.Model):
     )
     channel = models.ForeignKey(
         'ChannelNode',
-        related_name='contents',
         blank=True,
         null=True,
+        related_name='contents',
         on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        # TODO: extract title or smth equivalent in meta_info.
+        return '<Content %s> Channel %s' % (self.id, self.channel.title)
