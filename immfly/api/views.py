a25c40a7923f413ddea77644825e3faa2c95caa6
Abner Andino <brain.agam@gmail.com>
2020-03-03 20:32:24 +0100

    TASK: retrive all the channels and contents of the project

--- immfly/api/views.py
+++ immfly/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from api.serializers import ChannelNodeDetailSerializer, \
+    ContentDetailSerializer, TreeChannelDetailSerializer, LanguageDetailSerializer
+from channel.models import ChannelNode, Content, TreeChannel
+from common.models import Language
+
+
+class TreeChannelViewSet(ListCreateAPIView):
+
+    name = 'tree-view'
+    queryset = TreeChannel.objects.all()
+    serializer_class = TreeChannelDetailSerializer
+
+
+class ChannelNodeViewSet(ListCreateAPIView):
+ 
+    name = 'channel-node-view'
+    queryset = ChannelNode.objects.all()
+    serializer_class = ChannelNodeDetailSerializer
+
+
+class ContentViewSet(ListCreateAPIView):
+
+    name = 'content.view'
+    queryset = Content.objects.all()
+    serializer_class = ContentDetailSerializer
+
+
+class LanguageViewSet(ListCreateAPIView):
+
+    name = 'language-view'
+    queryset = Language.objects.all()
+    serializer_class = LanguageDetailSerializer
