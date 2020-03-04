27d6dbd30b23bf6ca7557a145c529594a501f31e
Abner Andino <aandino@externos.itnow.es>
2020-03-04 14:36:44 +0100

    BUG: creation of the sqlite3 when is not necessary

--- immfly/immfly/settings.py
+++ immfly/immfly/settings.py
@@ -80,12 +80,6 @@ WSGI_APPLICATION = 'immfly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 if DEBUG:
     DATABASES = {
